0f849fa90597aa54e8c0fccbd2b6a571b2a59ea2
Oleksii <lesha.avdeev@gmail.com>
2023-03-30 00:22:51 +0300

    Change initialization

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import Project from "./modules/Project";
-import ToDo from "./modules/To-do";
 import renderPage from "./modules/DOM";
 import "./style/style.css";
 
-function init() {
+(function init() {
   const projects = [Project("General")];
-  projects[0].addToDo(ToDo("Jason", "Maniac killer", "2023-03-29", 1, false));
-  projects[0].addToDo(ToDo("Jason", "Maniac killer", "2023-03-29", 2, false));
-  projects[0].addToDo(ToDo("Jason", "Maniac killer", "2023-03-29", 3, false));
-  projects[0].addToDo(ToDo("Jason", "Maniac killer", "2023-03-29", 4, false));
-  projects[0].addToDo(ToDo("Jason", "Maniac killer", "2023-03-29", 5, false));
 
   renderPage(projects);
-}
-
-init();
+})();
